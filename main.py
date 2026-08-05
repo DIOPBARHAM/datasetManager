16dd7b8ac047109704d2bc11d3f46a548ab86499
ibrahima <ibrahimajoob98@gmail.com>
2026-08-05 14:07:21 +0200

    initialiation du projet

---
+++ main.py
@@ -0,0 +1,9 @@
+# 3- Demandez à l utilisateur de saisir les métadonnées d un dataset
+
+nom = input("Nom du dataset : ")
+domaine = input("Domaine : ")
+lignes = int(input("Nombre de lignes : "))
+colonnes = int(input("Nombre de colonnes : "))
+taille = float(input("Taille en Mo : "))
+format_dataset = input("Format (csv ou json) : ")
+public = input("Dataset public (true ou false) : ")
